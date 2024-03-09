791d05eb430cd33b5e9db384726b8bd1cb2f9664
AleksandrKorCod <aksven66@gmail.com>
2024-03-09 12:27:01 +0300

    Optimize e2e/clients/dashboard-vue/App.vue

---
+++ e2e/clients/dashboard-vue/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <dashboard :JSIMg="JSIMg" />
+</template>
+
+<script>
+import Vue from 'vue'
+import JSIMg from '@JSIMg/core'
+import { Dashboard } from '@JSIMg/vue'
+
+export default {
+  name: 'App',
+  components: {
+    Dashboard,
+  },
+  computed: {
+    JSIMg: () => new JSIMg(),
+  },
+}
+</script>
+
+<style src="@JSIMg/core/dist/style.css"></style>
+<style src="@JSIMg/dashboard/dist/style.css"></style>
