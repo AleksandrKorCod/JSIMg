45cd537fced2a3f37a1c4d6b57bda2f4963f06e8
AleksandrKorCod <aksven66@gmail.com>
2024-09-22 02:46:01 +0300

    Fix off-by-one error in calculation_packages/@JSIMg/companion-client/src/index.js

---
+++ packages/@JSIMg/companion-client/src/index.js
@@ -0,0 +1,12 @@
+'use strict'
+
+/**
+ * Manages communications with Companion
+ */
+
+export { default as RequestClient } from './RequestClient.js'
+export { default as Provider } from './Provider.js'
+export { default as SearchProvider } from './SearchProvider.js'
+
+// TODO: remove in the next major
+export { default as Socket } from './Socket.js'
